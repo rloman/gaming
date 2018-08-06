58f1d0b9f16ea6d0117134cd8d0b109dd26d8ce1
Raymond Loman <raymond@carpago.nl>
2018-08-06 11:53:15 +0200

    Add BoardGame as @Entity

---
+++ src/main/java/nl/qien/gaming/domain/Boardgame.java
@@ -0,0 +1,48 @@
+package nl.qien.gaming.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Boardgame implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String name;
+    private int maxPlayers;
+    private boolean hasDice;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public boolean isHasDice() {
+        return hasDice;
+    }
+
+    public void setHasDice(boolean hasDice) {
+        this.hasDice = hasDice;
+    }
+}
